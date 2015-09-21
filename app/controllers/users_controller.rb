664138c18d5669f50f4cd0475feea10d71845c05
Ilias Tsangaris <iliastsangaris@gmail.com>
2015-09-21 12:27:00 -0700

    signup form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,4 +7,10 @@ class UsersController < ApplicationController
 
   def show
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :password_confirmation)
+  end
 end
